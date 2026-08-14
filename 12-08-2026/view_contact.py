a2f3cece28defb036ee77124b948827f16e1e51d
MASTER870-CMD <shashankgowdanb166@gmail.com>
2026-08-14 23:25:01 +0530

    updated the image and conetnet

---
+++ 12-08-2026/view_contact.py
@@ -0,0 +1,9 @@
+import re
+with open(r'c:\my files\A1technoksha (1)\project\12-08-2026\siemens-nx-cad.html', 'r', encoding='utf-8') as f:
+    content = f.read()
+
+match = re.search(r'<section.*?id="contact".*?>(.*?)</section>', content, re.DOTALL)
+if match:
+    lines = match.group(1).split('\n')
+    for line in lines[:80]:
+        print(line)
